16cea9a5c7ad0bc236cc3e06e4fd0ec85eaa7e8f
thanhson2000 <phansonbs2000@gmail.com>
2024-07-16 20:53:35 +0800

    修复catch的bug

--- src/main/java/practices/day7_05/JDBCConnection.java
+++ src/main/java/practices/day7_05/JDBCConnection.java
@@ -18,7 +18,8 @@ public class JDBCConnection {
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/itheima", "root", "27112222");
             Statement statement = connection.createStatement();
             statement.execute("INSERT INTO citizen() values (null,'007','Huong',27,'Bac Giang')");
-        } catch (SQLException ｜ ClassNotFoundException e) {
+            // 修复 catch 的bug
+        } catch (SQLException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         } 
     }
